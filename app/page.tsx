91d2f07424386d2b98e7559bdf419a50ba2e14fa
UchqunTurdiyev <uchqunjonturdiev@gmail.com>
2024-11-06 21:47:34 +0500

    cambridge

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<div className='2xl:w-[1400px] h-screen border-2 mx-auto max-2xl:px-6 bg-[url(https://themewagon.github.io/edu-meeting/assets/images/meetings-bg.jpg)] bg-no-repeat bg-cover'>
 			<h1 className='text-center pt-28 text-white/60 lg:text-6xl text-2xl'>
 				Samarqandda joylashgan{' '}
-				<span className='text-green-400'>Renessans school</span> xususiy maktabi
+				<span className='text-green-400'>Cambridge Unit</span> xususiy maktabi
 				uchun So&apos;rovnoma
 			</h1>
 			<div className='flex  w-full items-center justify-center lg:pt-20 pt-10'>
